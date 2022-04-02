043f14c493b5037a477680dd0494dcd7b7098113
Alice <disaster.woman007@gmail.com>
2022-04-02 15:02:56 +0300

    Carousel added

---
+++ src/data/SliderImages.jsx
@@ -0,0 +1,14 @@
+export const SliderImages = [
+    {
+        image: '/images/banner-carusel.png'
+    },
+    {
+        image: '/images/banner-2.jpg'
+    },
+    {
+        image: '/images/banner-3.jpg'
+    },
+    {
+        image: '/images/banner-4.jpg'
+    }
+]
---
+++ src/data/card-data.jsx
@@ -0,0 +1,12 @@
+const data = 
+[
+    {
+       id: 1,
+       title: "50s White wedding dress",
+       imageUrl:
+       "/public/images/dresses/1.jpg",
+       price: 7500
+
+    }
+]
+ 
